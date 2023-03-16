07536d52f86bc41bc0c1b762f05846b52cca8939
TetianaRudenko <fyilibratgif@gmail.com>
2023-03-16 14:53:10 +0200

    Update 01-color-switcher.js
    done task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.btnStart.addEventListener('click', onStartBtnClick);
+refs.btnStop.addEventListener('click', onStopBtnClick);
+
+refs.btnStop.disabled = true;
+
+let switcherBgColor = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick() {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+
+  switcherBgColor = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  refs.btnStop.disabled = true;
+  refs.btnStart.disabled = false;
+
+  clearInterval(switcherBgColor);
+}
+
+
+
 
