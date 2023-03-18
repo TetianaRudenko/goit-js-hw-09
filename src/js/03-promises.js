e903496cded75f286af2f84bab979da8b93b9551
TetianaRudenko <fyilibratgif@gmail.com>
2023-03-18 12:24:35 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
-function createPromise(position, delay) {
+/* function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
   } else {
     // Reject
   }
+} */
+
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onFormSubmit);
+
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`💙💛 ✔️ Fulfilled promise ${position} in ${delay}ms`);
+      }
+      reject(`⛔️ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+  });
+
 }
+
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  let DELAY = Number(form.delay.value);
+  const DELAY_STEP = Number(form.step.value);
+  const AMOUNT = Number(form.amount.value);
+
+  for (let i = 1; i <= AMOUNT; i += 1) {
+    createPromise(i, DELAY)
+      .then(result => Notify.success(result))
+      .catch(error => Notify.failure(error));
+    DELAY += DELAY_STEP;
+  }
+}
+
+
